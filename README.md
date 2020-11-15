014732cdc87642eda56a28cecc398430bcae6359
Ayman Bagabas <ayman.bagabas@gmail.com>
2020-11-14 20:05:10 -0500

    Use bmf2mof.wasm

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # WMIDumpper
 
-A tool that parses _WDG WMI method blocks to extract the WMI ACPI device mapping information.
+A set of tools to help analyze a WMI ACPI device mapping information and descriptors.
 
 ### References
 - [wmidump](https://github.com/iksaif/wmidump)
 - [Windows Instrumentation: WMI and ACPI](http://msdn.microsoft.com/en-us/windows/hardware/gg463463)
+- [bmfdec](https://github.com/pali/bmfdec)
--- main.js
+++ main.js
@@ -1,130 +1,147 @@
-const form = document.getElementById('form')
-
-form.addEventListener('submit', submit)
-
-const ACPI_WMI_EXPENSIVE   = 0x1
-const ACPI_WMI_METHOD      = 0x2	/* GUID is a method */
-const ACPI_WMI_STRING      = 0x4	/* GUID takes & returns a string */
-const ACPI_WMI_EVENT       = 0x8	/* GUID is an event */
+const ACPI_WMI_EXPENSIVE = 0x1
+const ACPI_WMI_METHOD = 0x2	/* GUID is a method */
+const ACPI_WMI_STRING = 0x4	/* GUID takes & returns a string */
+const ACPI_WMI_EVENT = 0x8	/* GUID is an event */
 
 class GuidBlock {
-    guid
-    object_id
-    instance_count
-    flags
+  guid
+  object_id
+  instance_count
+  flags
 
-    constructor(buffer) {
-        this.guid = buffer.slice(0, 16)
-        this.object_id = buffer.slice(16, 18)
-        this.instance_count = buffer[18]
-        this.flags = buffer[19]
-    }
+  constructor(buffer) {
+    this.guid = buffer.slice(0, 16)
+    this.object_id = buffer.slice(16, 18)
+    this.instance_count = buffer[18]
+    this.flags = buffer[19]
+  }
 
-    getGuid() {
-        return `${this.guid[3].toString(16).padStart(2, '0')+this.guid[2].toString(16).padStart(2, '0')+this.guid[1].toString(16).padStart(2, '0')+this.guid[0].toString(16).padStart(2, '0')}-${this.guid[5].toString(16).padStart(2, '0')+this.guid[4].toString(16).padStart(2, '0')}-${this.guid[7].toString(16).padStart(2, '0')+this.guid[6].toString(16).padStart(2, '0')}-${this.guid[8].toString(16).padStart(2, '0')+this.guid[9].toString(16).padStart(2, '0')}-${this.guid.slice(10).map(hex => hex.toString(16).padStart(2, '0')).join('')}`
-    }
+  getGuid() {
+    return `${this.guid[3].toString(16).padStart(2, '0') + this.guid[2].toString(16).padStart(2, '0') + this.guid[1].toString(16).padStart(2, '0') + this.guid[0].toString(16).padStart(2, '0')}-${this.guid[5].toString(16).padStart(2, '0') + this.guid[4].toString(16).padStart(2, '0')}-${this.guid[7].toString(16).padStart(2, '0') + this.guid[6].toString(16).padStart(2, '0')}-${this.guid[8].toString(16).padStart(2, '0') + this.guid[9].toString(16).padStart(2, '0')}-${this.guid.slice(10).map(hex => hex.toString(16).padStart(2, '0')).join('')}`
+  }
 
-    getNotifyId() {
-        return this.object_id[0].toString(16).padStart(2, '0')
-    }
-    
-    getReserved() {
-        return this.object_id[1].toString(16).padStart(2, '0')
-    }
+  getNotifyId() {
+    return this.object_id[0].toString(16).padStart(2, '0')
+  }
 
-    getInstanceCount() {
-        return this.instance_count.toString(16).padStart(2, '0')
-    }
+  getReserved() {
+    return this.object_id[1].toString(16).padStart(2, '0')
+  }
 
-    getFlags() {
-        return this.flags.toString(16).padStart(2, '0')
-    }
+  getInstanceCount() {
+    return this.instance_count.toString(16).padStart(2, '0')
+  }
 
-    toString() {
-        return `${this.getGuid()}
+  getFlags() {
+    return this.flags.toString(16).padStart(2, '0')
+  }
+
+  toString() {
+    return `${this.getGuid()}
         \tnotify_id: ${this.getNotifyId()}
         \treserved: ${this.getReserved()}
         \tinstance_count: ${this.getInstanceCount()}
         \tflags: ${this.getFlags()}`
-    }
+  }
 }
 
-function parseHexString(str) { 
-    var result = [];
-    while (str.length >= 8) { 
-        result.push(parseInt(str.substring(0, 8), 16));
+function parseHexString(str) {
+  var result = [];
+  while (str.length >= 8) {
+    result.push(parseInt(str.substring(0, 8), 16));
 
-        str = str.substring(8, str.length);
-    }
+    str = str.substring(8, str.length);
+  }
 
-    return result;
+  return result;
 }
 
-function readwdgin(str) {
+function readinput(str) {
     const buffer = []
     let comment = false
     let skipLine = false
     let byte = undefined
 
-    for(let i = 0; i < str.length; i++) {
-        // console.log(byte, str[i])
-        if (str[i] === '/' && str[i+1] && str[i+1] === '*') {
-            comment = true
-            i++
-            continue
-        }
-        if (str[i] === '*' && str[i+1] && str[i+1] === '/') {
-            comment = false
-            i++
-            continue
-        }
-        if (str[i] === '/' && str[i+1] && str[i+1] === '/') {
-            skipLine = true
-            i++
-            continue
-        }
-        if (comment) {
-            continue
-        }
-        if (skipLine) {
-            if (str[i] === '\n') {
-                skipLine = false
-            }
-            continue
-        }
-        if (!/^[a-zA-Z0-9]+$/i.test(str[i]) || i === str.length - 1) {
-            console.log('non alnum', str[i])
-            if (byte || byte === 0 || byte === '0') {
-                console.log('byte', byte)
-                buffer.push(parseInt(byte, 16))
-                byte = undefined
-            } else {
-                continue
-            }
-        } else {
-            console.log('alnum', str[i])
-            byte = byte ? byte + str[i] : str[i]
-        }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '/' && str[i + 1] && str[i + 1] === '*') {
+      comment = true
+      i++
+      continue
     }
+    if (str[i] === '*' && str[i + 1] && str[i + 1] === '/') {
+      comment = false
+      i++
+      continue
+    }
+    if (str[i] === '/' && str[i + 1] && str[i + 1] === '/') {
+      skipLine = true
+      i++
+      continue
+    }
+    if (comment) {
+      continue
+    }
+    if (skipLine) {
+      if (str[i] === '\n') {
+        skipLine = false
+      }
+      continue
+    }
+    isNum = /^[xXa-fA-F0-9]+$/i.test(str[i])
+    if (isNum) {
+      byte = byte ? byte + str[i] : str[i]
+    }
+    if (!isNum || i === str.length - 1){
+      if (byte || byte === 0 || byte === '0') {
+        buffer.push(parseInt(byte, 16))
+        byte = undefined
+      } else {
+        continue
+      }
+    }
+  }
 
-    return buffer
+  return new Uint8Array(buffer)
 }
 
 function parsewdgin(wdgs) {
-    const rv = []
-    var i,j,chunk = 20;
-    for (i=0,j=wdgs.length; i<j; i+=chunk) {
-        rv.push(wdgs.slice(i,i+chunk))
-    }
-    return rv.map(wdg => new GuidBlock(wdg))
+  const rv = []
+  var i, j, chunk = 20;
+  for (i = 0, j = wdgs.length; i < j; i += chunk) {
+    rv.push(wdgs.slice(i, i + chunk))
+  }
+  return rv.map(wdg => new GuidBlock(wdg))
 }
 
-function submit(event) {
-    event.preventDefault()
+function wdgsubmit(event) {
+  event.preventDefault()
 
-    const { wdgin, wdgout } = event.currentTarget
-    const wdgs = readwdgin(wdgin.value)
-    console.log(wdgs)
-    const rv = parsewdgin(wdgs).map(wdg => wdg.toString())
-    wdgout.value = rv.join('\n')
+  const { wdgin, wdgout } = event.currentTarget
+  const wdgs = readinput(wdgin.value)
+  const rv = parsewdgin(wdgs).map(wdg => wdg.toString())
+  wdgout.value = rv.join('\n')
+}
+
+function bmfsubmit(event) {
+  event.preventDefault()
+
+  const { bmfin, mofout } = event.currentTarget
+  mofout.value = ''
+  const bmfbuf = readinput(bmfin.value)
+
+  bmf2mof({
+    print: function (text) {
+      mofout.value += text + '\n'
+    }
+  }).then(module => {
+
+    const nByte = 2
+    function arrayToPtr(array) {
+      var ptr = module._malloc(array.length * nByte)
+      module.HEAPU8.set(array, ptr)
+      return ptr
+    }
+
+    module._parse_data(arrayToPtr(bmfbuf), bmfbuf.length)
+  })
 }
